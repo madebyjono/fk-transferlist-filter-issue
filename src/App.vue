ecefcba095f41f548377baed8055ce168b15db69
Jono <madebyjono.llp@gmail.com>
2024-09-21 12:59:19 +0800

    Working example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full">
-    <FormKit ref="allowedStoresForm" type="form" :actions="false" >
+    <FormKit v-if="!loading" ref="allowedStoresForm" type="form" :actions="false" >
 
       <FormKit id="allowed_stores" name="allowed_stores" type="transferlist" source-label="Live/Scheduled Stores"
         target-label="Allowed Stores" target-empty-message="No Stores Selected" :actions="false"
@@ -19,16 +19,41 @@ import { ref, onMounted } from 'vue';
 
 import { storesData } from '@/stores';
 const options = ref([]);
+const loading = ref(false);
 
 const customFilter = (option, search) => {
+  console.log("Search", search);  
   return option.label.toLowerCase().startsWith(search.toLowerCase())
 };
 
+ async function getData() {
+  const res = await fetch(
+    'https://142d19fe-7f5c-4bbf-9d9f-9538648560dc.mock.pstmn.io/utility/input-options?name=store_options'
+  )
+  if (res.ok) {
+    const data = await res.json()
+
+    if (data.options) {
+      options.value = data.options.map((option) => {
+        return {
+          value: option.value,
+          label: option.label,
+        }
+      })
+    }
+  }
+  return []
+}
+
+
 
 onMounted(async () => {
-  options.value = storesData;
-  
+  loading.value = true;
+ await getData();
+ loading.value = false;
 });
+
+
 
 </script>
 

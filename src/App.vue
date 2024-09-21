654b28bb5d446960c38958332cfa219db5cfca29
Jono <madebyjono.llp@gmail.com>
2024-09-21 11:37:31 +0800

    poking around

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
     <FormKit ref="allowedStoresForm" type="form" :actions="false" >
 
       <FormKit id="allowed_stores" name="allowed_stores" type="transferlist" source-label="Live/Scheduled Stores"
-        target-label="Allowed Stores" target-empty-message="No Stores Selected"
-        :options="options" target-searchable searchable :filter="customFilter" selected-icon="checkbox">
+        target-label="Allowed Stores" target-empty-message="No Stores Selected" :actions="false"
+        :options="options" target-searchable searchable selected-icon="checkbox"
+        :customFilter="customFilter"
+        >
       </FormKit>
 
     </FormKit>
@@ -13,8 +15,9 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import storesData from '@/stores.json';
+// import storesData from '@/stores.json';
 
+import { storesData } from '@/stores';
 const options = ref([]);
 
 const customFilter = (option, search) => {
@@ -23,7 +26,7 @@ const customFilter = (option, search) => {
 
 
 onMounted(async () => {
-  options.value = storesData.options;
+  options.value = storesData;
   
 });
 
